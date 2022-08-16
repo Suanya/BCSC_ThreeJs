853dbd5faede1c8c0c4a66242ee9ac55d01f6bd4
Sonja <sonjaunger@eyewant.me>
2022-08-16 10:44:16 -0700

    Problem solved
    yeah!

--- 09-geometries/exercise/src/script.js
+++ 09-geometries/exercise/src/script.js
@@ -13,7 +13,7 @@ const scene = new THREE.Scene()
 
 // Object
 const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xffff00 })
+const material = new THREE.MeshBasicMaterial({ color: 0xfff000 })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
