b49cca9f022f52ea9a62131c2f859602ed7ffc71
Sonja <sonjaunger@eyewant.me>
2022-08-17 11:27:55 -0700

    SuperSpin
    almostLive

--- 06-animation/exersice/06-animations/src/script.js
+++ 06-animation/exersice/06-animations/src/script.js
@@ -1,5 +1,7 @@
 import './style.css'
 import * as THREE from 'three'
+import vertexShader from './shader/vertex.glsl'
+import fragmentShader from './shader/fragment.glsl'
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
@@ -7,17 +9,35 @@ const canvas = document.querySelector('canvas.webgl')
 // Scene
 const scene = new THREE.Scene()
 
-// Object
+// Cube
 const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const material = new THREE.ShaderMaterial({
+    vertexShader: vertexShader,
+    fragmentShader: fragmentShader
+})
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
 // Sizes
 const sizes = {
-    width: 800,
-    height: 600
+    width: window.innerWidth,
+    height: window.innerHeight
 }
+
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
 
 // Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
@@ -29,4 +49,22 @@ const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
-renderer.render(scene, camera)
+
+// Clock
+const clock = new THREE.Clock()
+
+// Animations
+const tick = () =>
+{
+    // Clock 
+    const elapsedTime = clock.getElapsedTime()
+    
+    // Update Cube
+    mesh.rotation.y = elapsedTime * 0.2
+    
+    // Render
+    renderer.render(scene, camera)
+    
+    window.requestAnimationFrame(tick)
+}
+tick()
